e84d2d389484f9f90c9d6aaf4dc4be2274aedcdc
Erlangga Kencana <erlangga@transfez.com>
2024-03-20 21:41:18 +0700

    second commit

--- sumOfEven/sumOfEven.js
+++ sumOfEven/sumOfEven.js
@@ -1,5 +1,13 @@
 function sumOfEvens(numbers) {
-  // Your code here
+    // Your code here
+    let result = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        const number = numbers[i];
+        if (number % 2 === 0) {
+            result += number;
+        }
+    }
+    return result;
 }
 
 module.exports = sumOfEvens;
