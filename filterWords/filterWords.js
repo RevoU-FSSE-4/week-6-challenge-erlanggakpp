876837aa1d9ee3f4f113ccd284a4aeddc29eccd5
Erlangga Kencana <erlangga@transfez.com>
2024-03-21 10:27:00 +0700

    third commit

--- filterWords/filterWords.js
+++ filterWords/filterWords.js
@@ -1,5 +1,13 @@
 function filterWords(words) {
-  // Your code here
+    // Your code here
+    let result = [];
+    for (let index = 0; index < words.length; index++) {
+        const element = array[index];
+        if (words[i].length > 3) {
+            result.push(element);
+        }
+    }
+    return result;
 }
 
 module.exports = filterWords;
