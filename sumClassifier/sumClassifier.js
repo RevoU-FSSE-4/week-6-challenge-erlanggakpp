22d026f7c81b7682a31cc58697f98bc0882445d8
Erlangga Kencana <erlangga@transfez.com>
2024-03-21 20:17:18 +0700

    sixth commit

--- sumClassifier/sumClassifier.js
+++ sumClassifier/sumClassifier.js
@@ -1,5 +1,24 @@
 function sumClassifier(numbers) {
-  // Your code here
+    // Your code here
+    let sumOfNumber = 0;
+    for (let index = 0; index < numbers.length; index++) {
+        const number = numbers[index];
+        sumOfNumber += number;
+    }
+    let result = "Large Sum";
+    switch (true) {
+        case sumOfNumber === 10:
+            result = "Equal Sum";
+            break;
+        case sumOfNumber < 10:
+            result = "Small Sum";
+            break;
+    }
+    return result;
 }
+
+console.log(sumClassifier([1, 2, 3, 5])); // Output: "Large Sum"
+console.log(sumClassifier([5, 5])); // Output: "Equal Sum"
+console.log(sumClassifier([0, 1, 2])); // Output:
 
 module.exports = sumClassifier;
